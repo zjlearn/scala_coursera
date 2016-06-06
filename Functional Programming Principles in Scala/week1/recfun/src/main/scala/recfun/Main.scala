c2a95153417836c69ae1f08a5af47478c7aedf7c
zjlearn <1241225106@qq.com>
2016-06-06 18:37:12 +0800

    complete th assignment one

--- Functional Programming Principles in Scala/week1/recfun/src/main/scala/recfun/Main.scala
+++ Functional Programming Principles in Scala/week1/recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,31 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+    if(c==0 || c==r) return 1;
+    else return pascal(c-1,r-1)+pascal(c,r-1);
+  }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def helper(flag:Int, chars:List[Char]):Boolean={
+        if(chars.isEmpty) return flag==0;
+        if(chars.head=='(' )helper(flag+1, chars.tail)
+        else  if (chars.head==')')  return ( flag > 0 && helper(flag-1, chars.tail));
+        else helper(flag, chars.tail);
+      }
+    helper(0,chars)
+  }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+        if(coins.isEmpty || money <0) return 0;
+        if(money==0) return 1;
+        countChange(money,coins.tail)+ countChange(money-coins.head, coins)
+    }
   }
+
---
+++ Functional Programming Principles in Scala/week1/recfun/src/main/scala/recfun/Test.scala
@@ -0,0 +1,9 @@
+package recfun
+
+/**
+  * Created by zj on 2016/6/6.
+  */
+object Test  extends  App{
+  val result=Main.balance(":-)".toList);
+  print(result)
+}
